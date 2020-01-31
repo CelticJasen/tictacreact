54a38b9857f444fe93dfdacca0d181ec511e848c
FatedSoul <celticjasen@hotmail.com>
2020-01-31 16:04:43 -0600

    Updates

---
+++ precache-manifest.5c39f2095e5a39095b6e56304b1abe28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6143d8bc095c6ed50fbc92b59dd4162",
+    "url": "/tictacreact/index.html"
+  },
+  {
+    "revision": "a7b8c9877d788b953cec",
+    "url": "/tictacreact/static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "c15a60108eaf59ad17b6",
+    "url": "/tictacreact/static/js/2.a456b383.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tictacreact/static/js/2.a456b383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b8c9877d788b953cec",
+    "url": "/tictacreact/static/js/main.a7e833f3.chunk.js"
+  },
+  {
+    "revision": "1ed1a934047a339999f1",
+    "url": "/tictacreact/static/js/runtime-main.f22f5158.js"
+  }
+]);
